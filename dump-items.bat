fd915dd7a32754e33e9efd892fcdf90d4ab793e8
Windyboy <y765432100@hotmail.com>
2023-09-03 10:36:47 +0800

    improve CLASSPATH in batch file

--- dump-items.bat
+++ dump-items.bat
@@ -1,5 +1,5 @@
 @echo off
-@title Dump
-set CLASSPATH=.;dist\Lithium.jar;dist\mina-core.jar;dist\slf4j-api.jar;dist\slf4j-jdk14.jar;dist\mysql-connector-java-bin.jar
+@title Dump Item
+set CLASSPATH=.;dist\*
 java -server -Dnet.sf.odinms.wzpath=wz\ tools.wztosql.DumpItems
 pause
