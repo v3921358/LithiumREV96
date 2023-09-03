46c2ed980244bdca7f0fed1854cb1735a7c3d02e
Windyboy <y765432100@hotmail.com>
2023-09-03 10:36:23 +0800

    Make batch files use relative wzpaths

--- dump-items.bat
+++ dump-items.bat
@@ -1,5 +1,5 @@
 @echo off
 @title Dump
 set CLASSPATH=.;dist\Lithium.jar;dist\mina-core.jar;dist\slf4j-api.jar;dist\slf4j-jdk14.jar;dist\mysql-connector-java-bin.jar
-java -server -Dnet.sf.odinms.wzpath=D:\XMLWZs\GMSv111\ tools.wztosql.DumpItems
+java -server -Dnet.sf.odinms.wzpath=wz\ tools.wztosql.DumpItems
 pause
