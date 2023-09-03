d1874dacee1b94f74323283340b89424b32a88a1
Windyboy <y765432100@hotmail.com>
2023-09-03 15:15:42 +0800

    fix soft DC

--- src/handling/mina/MaplePacketEncoder.java
+++ src/handling/mina/MaplePacketEncoder.java
@@ -21,50 +21,48 @@
 package handling.mina;
 
 import client.MapleClient;
-
+import org.apache.mina.common.ByteBuffer;
+import org.apache.mina.common.IoSession;
+import org.apache.mina.filter.codec.ProtocolEncoder;
+import org.apache.mina.filter.codec.ProtocolEncoderOutput;
 import tools.MapleAESOFB;
 import tools.MapleCustomEncryption;
 
 import java.util.concurrent.locks.Lock;
 
-import org.apache.mina.common.ByteBuffer;
-import org.apache.mina.common.IoSession;
-import org.apache.mina.filter.codec.ProtocolEncoder;
-import org.apache.mina.filter.codec.ProtocolEncoderOutput;
-
 public class MaplePacketEncoder implements ProtocolEncoder {
 
     @Override
     public void encode(final IoSession session, final Object message, final ProtocolEncoderOutput out) throws Exception {
-	final MapleClient client = (MapleClient) session.getAttribute(MapleClient.CLIENT_KEY);
+        final MapleClient client = (MapleClient) session.getAttribute(MapleClient.CLIENT_KEY);
 
-	if (client != null) {
-	    final MapleAESOFB send_crypto = client.getSendCrypto();
+        if (client != null) {
+            final MapleAESOFB send_crypto = client.getSendCrypto();
 
-	    final byte[] inputInitialPacket = ((byte[]) message);
-	    final byte[] unencrypted = new byte[inputInitialPacket.length];
-	    System.arraycopy(inputInitialPacket, 0, unencrypted, 0, inputInitialPacket.length); // Copy the input > "unencrypted"
-	    final byte[] ret = new byte[unencrypted.length + 4]; // Create new bytes with length = "unencrypted" + 4
+            final byte[] inputInitialPacket = ((byte[]) message);
+            final byte[] unencrypted = new byte[inputInitialPacket.length];
+            System.arraycopy(inputInitialPacket, 0, unencrypted, 0, inputInitialPacket.length); // Copy the input > "unencrypted"
+            final byte[] ret = new byte[unencrypted.length + 4]; // Create new bytes with length = "unencrypted" + 4
 
-	    final Lock mutex = client.getLock();
-	    mutex.lock();
-	    try {
-		final byte[] header = send_crypto.getPacketHeader(unencrypted.length);
-		MapleCustomEncryption.encryptData(unencrypted); // Encrypting Data
-		send_crypto.crypt(unencrypted); // Crypt it with IV
-		System.arraycopy(header, 0, ret, 0, 4); // Copy the header > "Ret", first 4 bytes
-	    } finally {
-		mutex.unlock();
-	    }
-	    System.arraycopy(unencrypted, 0, ret, 4, unencrypted.length); // Copy the unencrypted > "ret"
-	    out.write(ByteBuffer.wrap(ret));
-	} else { // no client object created yet, send unencrypted (hello)
-	    out.write(ByteBuffer.wrap(((byte[]) message)));
-	}
+            final Lock mutex = client.getLock();
+            mutex.lock();
+            try {
+                final byte[] header = send_crypto.getPacketHeader(unencrypted.length);
+                MapleCustomEncryption.encryptData(unencrypted); // Encrypting Data
+                send_crypto.crypt(unencrypted); // Crypt it with IV
+                System.arraycopy(header, 0, ret, 0, 4); // Copy the header > "Ret", first 4 bytes
+            } finally {
+                mutex.unlock();
+            }
+            System.arraycopy(unencrypted, 0, ret, 4, unencrypted.length); // Copy the unencrypted > "ret"
+            session.write(ByteBuffer.wrap(ret));
+        } else { // no client object created yet, send unencrypted (hello)
+            out.write(ByteBuffer.wrap(((byte[]) message)));
+        }
     }
 
     @Override
     public void dispose(IoSession session) throws Exception {
-	// nothing to do
+        // nothing to do
     }
 }
